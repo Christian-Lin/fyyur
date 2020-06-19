c58f4bb694be67234fc8a34bb67596886215b05c
Christian Lin <christianlin@my.com>
2020-06-19 13:30:14 +0100

    Initial commit
    Starter code from Udacity's repository

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off warning
+
+# Connect to the database
+# Proxy password generated for project (Windows requirement)
+# IMPORTANT: if migrate does not work ("target db not up to date"), run stamp head to update head:
+            # $ flask db stamp head
+            # $ flask db migrate
+            # $ flask db upgrade
+# TODO IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = 'postgres://chris:admin@localhost:5432/fyyur'
